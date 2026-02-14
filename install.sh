ad8049967349d99ef545eb0302e20bf498241fb4
lignigno <nik.fomin.2001@list.ru>
2026-02-14 18:49:52 +0500

    fix syntax error

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ PROJECT_DIR="$HOME/uploader"
 
 go build -o "$PROJECT_DIR/uploader" "$PROJECT_DIR/main.go"
 
-rm -rf $PROGECT_DIR/.git*
-rm -rf $PROGECT_DIR/imgs
-rm -rf $PROGECT_DIR/README.md
-rm -rf $PROGECT_DIR/main.go
+rm -rf $PROJECT_DIR/.git*
+rm -rf $PROJECT_DIR/imgs
+rm -rf $PROJECT_DIR/README.md
+rm -rf $PROJECT_DIR/main.go
 
-echo "alias uploader=\"$PROGECT_DIR/uploader\"" >> "$HOME/.zshrc"
-echo "alias uploader=\"$PROGECT_DIR/uploader\"" >> "$HOME/.bashrc"
+echo "alias uploader=\"$PROJECT_DIR/uploader\"" >> "$HOME/.zshrc"
+echo "alias uploader=\"$PROJECT_DIR/uploader\"" >> "$HOME/.bashrc"
 
 
