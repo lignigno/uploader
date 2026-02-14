ff90b9b19d71f936a47f95368aa1c06318a7dacd
lignigno <nik.fomin.2001@list.ru>
2026-02-14 20:40:05 +0500

    fix installer

--- install.sh
+++ install.sh
@@ -4,11 +4,12 @@ PROJECT_DIR="$HOME/uploader"
 
 go build -o "$PROJECT_DIR/uploader" "$PROJECT_DIR/main.go"
 
-rm -rf $PROJECT_DIR/.git*
 rm -rf $PROJECT_DIR/imgs
-rm -rf $PROJECT_DIR/README.md
-rm -rf $PROJECT_DIR/main.go
+rm -rf $PROJECT_DIR/.git*
+rm -rf $PROJECT_DIR/.gitignore
 rm -rf $PROJECT_DIR/install.sh
+rm -rf $PROJECT_DIR/main.go
+rm -rf $PROJECT_DIR/README.md
 
 echo "alias uploader=\"$PROJECT_DIR/uploader\"" >> "$HOME/.zshrc"
 echo "alias uploader=\"$PROJECT_DIR/uploader\"" >> "$HOME/.bashrc"
